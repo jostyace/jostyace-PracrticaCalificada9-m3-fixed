92449632355d90cb077568f2d8d3b0f4cf6da7ad
jostyace <jostyace@gmail.com>
2024-05-06 11:07:03 -0500

    Upload Fixed Repo

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+export function Footer () {
+
+  return (
+    <footer className='footer'>
+      <h4>Practica Calificada 8 <span>Funval</span></h4>
+      <h5>Practica de useContext & useReducer</h5>
+    </footer>
+  )
+}
